a1dd04790b02b0274ae2f6d9e9286cd0fc00ddc8
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-03 09:07:24 +0800

    initial commit

---
+++ mp-test/ItemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCollectionViewCell.swift
+//  mp-test
+//
+//  Created by Francis Jemuel Bergonia on 5/3/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
