601ba75c3bb82d9ab784d2b1c695d31b981eee17
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-03 10:00:41 +0800

    collectionViewLayout set

--- mp-test/ViewController.swift
+++ mp-test/ViewController.swift
@@ -8,13 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Item {
+    var imageName: String
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items: [Item] = [Item(imageName: "1"),
+                         Item(imageName: "2"),
+                         Item(imageName: "3"),
+                         Item(imageName: "4"),
+                         Item(imageName: "5"),
+                         Item(imageName: "6"),
+                         Item(imageName: "7"),
+                         Item(imageName: "8"),
+                         Item(imageName: "9"),
+                         Item(imageName: "10"),
+                         Item(imageName: "11"),
+                         Item(imageName: "12")]
+    
+    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    let cellIdentifier = "ItemCollectionViewCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionView()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.setupCollectionViewItemSize()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let nib = UINib(nibName: "ItemCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: cellIdentifier)
+    }
+
+    private func setupCollectionViewItemSize() {
+        if collectionViewFlowLayout == nil {
+            let numberOfItemPerRow: CGFloat = 3
+            let lineSpacing: CGFloat = 5
+            let interItemSpacing: CGFloat = 5
+            
+            let width = (collectionView.frame.width - (numberOfItemPerRow - 1) * interItemSpacing) / numberOfItemPerRow
+            let height = width
+            
+            collectionViewFlowLayout = UICollectionViewFlowLayout()
+            
+            collectionViewFlowLayout.itemSize = CGSize(width: width, height: height)
+            collectionViewFlowLayout.sectionInset = UIEdgeInsets.zero
+            collectionViewFlowLayout.scrollDirection = .vertical
+            collectionViewFlowLayout.minimumLineSpacing = lineSpacing
+            collectionViewFlowLayout.minimumInteritemSpacing = interItemSpacing
+            
+            collectionView.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource  {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ItemCollectionViewCell
+        
+        cell.imageView.image = UIImage(named: items[indexPath.item].imageName)
+        
+        return cell
+    }
+    
+    
+}
