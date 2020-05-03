8b8d264c93ae49bb59d7d52ca24123c26c2c874a
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-03 08:58:08 +0800

    Initial Commit

---
+++ mp-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mp-test
+//
+//  Created by Francis Jemuel Bergonia on 5/3/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
